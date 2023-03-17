07ce9513952d972466968cfbc51f0f08e3d49b2e
shady2532 <shadyrizk2532@gmail.com>
2023-03-18 01:48:36 +0200

    numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,19 +15,18 @@ int main(void)
 	{
 		for (j = 0 ; j < 100 ; j++)
 		{
-			if(i<j)
+			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i == 98 && j == 99 )
+				if (i == 98 && j == 99)
 					continue;
 				putchar(',');
 				putchar(' ');
 			}
-		
 		}
 	}
 	putchar('\n');
