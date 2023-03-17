94a15e5e5e02aa4c9d62764f63ce11206e5a0269
shady2532 <shadyrizk2532@gmail.com>
2023-03-18 00:40:01 +0200

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char c = 'a';
+	for(int i = 0 ; i < 26 ; i++)
+	{
+		putchar(c+i);
+	}
+	putchar('\n');
+	return (0);
+}
