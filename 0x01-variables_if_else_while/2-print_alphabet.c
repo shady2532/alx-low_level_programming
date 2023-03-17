652c89cdcdd4328be4701d330c597865c29b1742
shady2532 <shadyrizk2532@gmail.com>
2023-03-18 00:44:04 +0200

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char c = 'a';
 	int i;
-	for(i = 0 ; i < 26 ; i++)
+
+	for (i = 0 ; i < 26 ; i++)
 	{
-		putchar(c+i);
+		putchar(c + i);
 	}
 	putchar('\n');
 	return (0);
