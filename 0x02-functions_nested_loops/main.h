7c227e82e9af553a9a64c29809c8e0ebe35ef354
Shady2532 <89049454+shady2532@users.noreply.github.com>
2023-03-22 00:56:17 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "_putchar.c"
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,4 +11,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
