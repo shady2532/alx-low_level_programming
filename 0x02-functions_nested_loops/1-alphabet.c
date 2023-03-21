80c74cffc5851eb2f720df208c75ad851ae39a84
shady2532 <shadyrizk2532@gmail.com>
2023-03-22 01:51:11 +0200

    lol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
+//I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game 
 void print_alphabet(void)
 {
 	int i;
 
-	for(i = 97 ; i < 122 ; i++)
+	for (i = 97 ; i < 123 ; i++)
 	{
 		putchar(i);
 	}
