f614b1c98b013721f822ea460aaea22126610da2
shady2532 <shadyrizk2532@gmail.com>
2023-03-22 03:20:18 +0200

    hi

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns last digit of n.
+ * @n: character variable to be checked.
+ * Return: returns last digit of n.
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
