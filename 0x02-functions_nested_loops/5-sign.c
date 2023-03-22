57c39b6749d2b56a1c65ce6d3cad96e311efcc57
shady2532 <shadyrizk2532@gmail.com>
2023-03-22 03:02:08 +0200

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		putchar('1');
+		return("+1");
 	}
 	else if (n == 0)
 	{
-		putchar('0');
-		putchar('0');
+		return("00");
 	}
 	else 
 	{
-		putchar('-');
-		putchar('1');
+		return("-1");
 	}
 	return;
 }
