a946cb7da7433e6cc62a36bab339e82c6d7c53a1
shady2532 <shadyrizk2532@gmail.com>
2023-03-22 02:28:27 +0200

    aaaaa

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,15 @@
 
 /**
  * _islower - checks for lowercase character
+ * c - character variable
+ * return - returns 1 if c is a lower case character, otherwise returns 0
+ * 
  */
 
 int _islower(int c)
 {
-	 if (c >= 97 && c <= 122)
-	 return (1);
-	 else
-	 return (0);
+	if (c >= 97 && c <= 122)
+	   	return (1);
+	else
+		return (0);
 }
