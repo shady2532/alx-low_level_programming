68881c8590862dd2cafb2dd5519ebf77a1677673
shady2532 <shadyrizk2532@gmail.com>
2023-03-22 03:16:05 +0200

    hi

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - checks if n is positive or negative or zero.
+ * @n: character variable to be checked.
+ * Return: returns 1 if c is positive, returns -1 if c is negative.
+ */
+
+int _abs(int n)
+{
+	return (n);
+}
