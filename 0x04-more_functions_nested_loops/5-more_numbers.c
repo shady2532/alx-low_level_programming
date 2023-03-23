8552724755cfcf87f8b0e95f56c6ce8cd7253b1b
shady2532 <shadyrizk2532@gmail.com>
2023-03-23 18:39:08 +0200

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	for (i = '0'; i <= '4'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
