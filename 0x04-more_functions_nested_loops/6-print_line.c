bd79adb73266bc9c47dc07d109504e8e1c67c3a3
shady2532 <shadyrizk2532@gmail.com>
2023-03-23 18:51:31 +0200

    new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff?
+ */
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
