cfd817d5e5bf47db45945b852955535ec7622356
shady2532 <shadyrizk2532@gmail.com>
2023-03-23 19:33:56 +0200

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints triangle
+ *
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - 1 - i; j > 0; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i + 1; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
