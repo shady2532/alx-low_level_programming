1003c6374d83d063023f1d2c6cafac661af202ae
Shady2532 <89049454+shady2532@users.noreply.github.com>
2023-03-23 17:34:06 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 
 /*
- * File: main.h 
+ * File: main.h
+ *
  * Desc: header file
 **/
+
 void positive_or_negative(int i);
-int largest_number(int a,int b,int c);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
