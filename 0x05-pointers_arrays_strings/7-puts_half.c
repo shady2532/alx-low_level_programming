08e4d10d5e5383e1bced82dc297be459cfa1838f
shady2532 <shadyrizk2532@gmail.com>
2023-03-28 03:50:43 +0200

    seventh

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,8 +8,11 @@
 void puts_half(char *str)
 {
 	int i;
+	int even = 0;
 
-	for (i = _strlen(str) / 2; str[i] != '\0'; i++)
+	if (_strlen(str) % 2 != 0)
+		even += 1;
+	for (i = (_strlen(str) + even) / 2; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
