3b6f37834d3bccb2f62018d610d9fdbb24bdba3c
Shady2532 <89049454+shady2532@users.noreply.github.com>
2023-03-30 01:14:58 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,22 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strcat - concatenates two strings
  * @dest: input parameter string
  * @src: input parameter string
  * Return: dest
 **/
+
 char *_strcat(char *dest, char *src)
 {
-	int destLen = strlen(dest);
+	int dest_len = strlen(dest);
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-		dest[destLen + 1] = src[i];
+		dest[dest_len + i] = src[i];
 
-	dest[destLen + 1] = '\0';
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
