0a5aaa90e8c1ba57fb147fbc00511f8cb989c9a6
shady2532 <shadyrizk2532@gmail.com>
2023-03-29 14:57:47 +0200

    1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ * Return: dest
+**/
+char *_strcat(char *dest, char *src)
+{
+	int destLen = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[destLen + 1] = src[i];
+	dest[destLen + 1] = '\0';
+
+	return (dest);
+}
