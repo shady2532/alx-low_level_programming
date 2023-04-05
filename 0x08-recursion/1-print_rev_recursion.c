521d5aefecfbf4c2bf05adca01d5b2146399bcc0
shady2532 <shadyrizk2532@gmail.com>
2023-04-06 00:52:04 +0200

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - prints in reverse recurse
+ * @s: string to recurse
+ */
+int i = 1;
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s - i);
+		_putchar(*s);
+	}
+}
