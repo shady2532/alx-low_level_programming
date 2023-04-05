64f2387923065c20548fda1f09e9f34a04328d12
shady2532 <shadyrizk2532@gmail.com>
2023-04-06 01:36:05 +0200

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * sqrt_a - Main Entry
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt_a(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns square root
+ * @n: integer to return
+ * Return: returns int of squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	return sqrt_a(n, 0);
+}
