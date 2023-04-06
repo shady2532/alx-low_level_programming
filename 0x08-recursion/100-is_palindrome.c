44bfe496f2d708ecfb3bb1c81f35023e6b177022
shady2532 <shadyrizk2532@gmail.com>
2023-04-06 02:16:40 +0200

    7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+int is_palindrome(char *s)
+{
+	int i;
+	int n = strlen(s);
+	int counter = 0;
+
+	for (i=0; i<n/2; i++)
+	{
+		if (s[i]==s[n-i-1])
+			counter++;
+	}
+	if (counter == n/2)
+		return (1);
+	else
+		return (0);
+}
