14249cb3539f6774085d0426cf8878ea6338db2e
shady2532 <shadyrizk2532@gmail.com>
2023-04-06 02:39:44 +0200

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@ int compare(char *s, int n)
 	if (n <= 0)
 		return (1);
 	if (*s == s[n - 1])
-		return (compare(s + 1, n--));
+		return (compare(s + 1, n - 2));
 	else
 		return (0);
 }
@@ -15,7 +15,7 @@ int compare(char *s, int n)
 
 int is_palindrome(char *s)
 {
-	int n = strlen(s) / 2;
+	int n = strlen(s);
 
 	return (compare(s, n));
 }
