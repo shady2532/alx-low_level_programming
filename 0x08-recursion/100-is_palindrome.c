3e8bda2c33317673249a7cbb2308e6d67e1dafc8
shady2532 <shadyrizk2532@gmail.com>
2023-04-06 02:22:24 +0200

    7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,17 +2,32 @@
 #include <string.h>
 #include <stdio.h>
 
+int counting(char *s, int n, int i)
+{
+	int counter = 0;
+
+	if (s[i] == s[n - 1 - i])
+	{
+		counter = counting (s + 1, n, i++);
+		return (counter++);
+	}
+}
+
+
 int is_palindrome(char *s)
 {
 	int i;
 	int n = strlen(s);
 	int counter = 0;
 
-	for (i=0; i<n/2; i++)
+	/*for (i=0; i<n/2; i++)
 	{
 		if (s[i]==s[n-i-1])
 			counter++;
-	}
+	}*/
+	
+
+
 	if (counter == n/2)
 		return (1);
 	else
