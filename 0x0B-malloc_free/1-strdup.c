6fce8f992ebdeb61c535a504bb3f931511acee5d
shady2532 <shadyrizk2532@gmail.com>
2023-04-12 01:21:04 +0200

    hi

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Main Entry
+ * @str: input
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *nstr;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	nstr = malloc(sizeof(char) * (len));
+
+	if (nstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		nstr[i] = str[i];
+
+	nstr[len] = '\0';
+
+	return (nstr);
+}
