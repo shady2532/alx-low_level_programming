1cd4e36dac9fa82738676fc0f1bc2caac7a85368
shady2532 <shadyrizk2532@gmail.com>
2023-04-11 02:44:09 +0200

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
