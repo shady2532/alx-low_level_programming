d04e5630ada82eba7826b4c0c49d8194eae72d1f
shady2532 <shadyrizk2532@gmail.com>
2023-04-12 01:13:17 +0200

    hi

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,21 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 
 	arr = malloc(sizeof(char) * size);
 
 	if (arr = NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
+	{
 		arr[i] = c;
+	}
 
 	return (arr);
 }
