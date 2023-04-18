73ac7f1c01c3cd2fd7b603eb4e93d3335265c1cb
shady2532 <shadyrizk2532@gmail.com>
2023-04-18 02:07:10 +0200

    hi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s)
+int _strlen(char *s);
 #endif
