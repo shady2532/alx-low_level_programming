798751ecae27d964054b4b0978791333a3cc0f9a
shady2532 <shadyrizk2532@gmail.com>
2023-04-28 04:28:07 +0300

    updated

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: pointer to list
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+    while (head)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
+    }
+}
