eac42a6c31a28708672fc16d9431d8162ad68001
shady2532 <shadyrizk2532@gmai.com>
2023-05-04 19:29:05 +0300

    updated

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
     while (mul > 0)
     {
-        bits++;
+        bits += (mul & 1);
         mul >>= 1;
     }
     return (bits);
